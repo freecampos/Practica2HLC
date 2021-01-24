b6d798bde4d346e1bc331f3acea9fb703d522e01
inmamesa <77283757+inmamesa@users.noreply.github.com>
2021-01-24 12:58:23 +0100

    modificación js

---
+++ scripts/VSCJavaScript.js
@@ -0,0 +1,12 @@
+<script>
+	$(document).ready(function)
+	{
+		$('.zoom').hover(function()
+		{
+			$(this).addClass('transition');
+		}, function()
+		{
+			$(this).removeClass('transition');
+		});
+	});
+</script>
