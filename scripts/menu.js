a6d321098b44fadbf8d95756b2e77bc48ffe4d2b
freecampos <freecampos@users.noreply.github.com>
2021-01-24 13:16:36 +0100

    Menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,12 @@
-//$(document).ready(function(){
-	//var altura = $('.menu').offset().top; /*Altura que hay entre la aprte superior y el menu*/
-	
-	//alert(altura);
-	/*Si hace scroll y pasamos de la altura de donde se encuentra el menu fijo, nos acompaña durante el scroll*/
-	$(window).on("scroll", funtion(){
+$(document).ready(function () {
+	var altura = $('#header-framework nav').offset().top;
+
+	$(window).on('scroll', function () {
 		if($(window).scrollTop() > altura){
-			$('nav').addClass('menu-fixed');
-		} else{
-			$('nav').removeClass('menu-fixed');
+			$('#header-framework nav').addClass('menu-fixed');
 		}
-	});
-//});
+		else{
+			$('#header-framework nav').removeClass('menu-fixed');
+		}
+	})
+});
