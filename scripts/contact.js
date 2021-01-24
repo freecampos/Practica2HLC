6dc965dd09355a7012e4d3e51f27a6557bf0ed49
freecampos <freecampos@users.noreply.github.com>
2021-01-24 17:22:46 +0100

    Formulario

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,69 +1,60 @@
-function comprobar_blancos() {
-	//Variables
-	var nombre = document.getElementById('nombre').value;
-	var apellidos = document.getElementById('apellidos').value;
-	var email = document.getElementById('email').value;
-	var telefono = document.getElementById('telefono').value;
-	var fecha = document.getElementById('fecha').value;
+function comprobarBlancos() {
+	var nombre = document.getElementById("nombre-contacto").value;
+	var apellidos = document.getElementById("apellidos-contacto").value;
+	var email = document.getElementById("email-contacto").value;
+	var telefono = document.getElementById("telefono-contacto").value;
+	var fecha = document.getElementById("fecha-nac").value;
+	var bandera = false;
 
-	var error = false; /*Bandera*/
-
-	alert("Entra aqui");
-
-	if(nombre=="" || apellidos=="" || email=="" || telefono=="" || fecha=""){
-		error = true; /*AL haber campos en blanco, saltamos el error*/
+	if(nombre == "" || apellidos == "" || email == "" || telefono == "" || fecha == ""){
+		bandera = true;
 		alert("Debe rellenar todos los campos del formulario para ser enviado.");
 	}
 	else{
-		/*No hay ningun campo vacio*/
-		resetearCampos(nombre, apellidos, email, telefono, fecha);
 
-		if(isNaN(telefono) || cantidadNumeros(telefono)){
+		if(isNaN(telefono)){
 			/*No es un numero, o faltan numeros para completar un numero de telefono.*/
-			error = true;
+			bandera = true;
 			alert("Número de telefono no valido.");
 		}
 
-		if(!isNaN(fecha)){
-			/*No es correcta la fecha*/
-			error = true;
-			alert("Fecha incorrecta.");
-		}
+		bandera = comprobarChecks();
 
-		comprobarCheck();
-
-		if(error = false)
+		if(bandera){
 			resetearCampos();
+		}
+		else{
+			alert("Solicitud correcta.");
+			resetearCampos();
+		}
+		
 	}
 }
 
-function comprobarCheck() {
-	var java = document.getElementById('cb-java').checked;
-	var db = document.getElementById('cb-db').checked;
-	var python = document.getElementById('cb-python').checked;
-	var html = document.getElementById('cb-html').checked;
-	var css = document.getElementById('cb-css').checked;
+function comprobarChecks() {
+	var java = document.getElementById("cb-java").checked;
+	var db = document.getElementById("cb-db").checked;
+	var python = document.getElementById("cb-python").checked;
+	var html = document.getElementById("cb-html").checked;
+	var css = document.getElementById("cb-css").checked;
 
-	if(!java && !db && !python && !html && !css)
+	if(!java && !db && !python && !html && !css){
 		alert("No cumples con los requisitos mínimos.");
+		return true;
+	}
+
+	return false;
 }
 
 function resetearCampos() {
-	document.getElementById('nombre').value = "";
-	document.getElementById('apellidos').value = "";
-	document.getElementById('email').value = "";
-	document.getElementById('telefono').value = "";
-	document.getElementById('fecha').value = "";
-	document.getElementById('cb-java').checked = false;
-	document.getElementById('cb-db').checked = false;
-	document.getElementById('cb-python').checked = false;
-	document.getElementById('cb-html').checked = false;
-	document.getElementById('cb-css').checked = false;
-}
-
-function cantidadNumeros(telefono) {
-	if(telefono.toString().length == 9)
-		return 1;
-	else 
-		return 0;
+	document.getElementById("nombre-contacto").value = "";
+	document.getElementById("apellidos-contacto").value = "";
+	document.getElementById("email-contacto").value = "";
+	document.getElementById("telefono-contacto").value = "";
+	document.getElementById("fecha-nac").value = "";
+	document.getElementById("cb-java").checked = false;
+	document.getElementById("cb-db").checked = false;
+	document.getElementById("cb-python").checked = false;
+	document.getElementById("cb-html").checked = false;
+	document.getElementById("cb-css").checked = false;
 }
